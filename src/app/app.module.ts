71d8e74803edfc8609f3a9f4850208f069fa2441
gregrossdev <gregross.dev@gmail.com>
2022-04-15 13:48:55 -0400

    forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { ProductItemComponent } from './product-item.component';
 import { ProductItemListComponent } from './product-item-list.component';
+import { ProductItemFormComponent } from './product-item-form.component';
+
 import { FavoriteDirective } from './favorite.directive';
 import { CategoryListPipe } from './category-list.pipe';
-import { ProductItemFormComponent } from './product-item-form.component';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
   ],
   declarations: [
     AppComponent,
     ProductItemComponent,
     ProductItemListComponent,
+    ProductItemFormComponent,
     FavoriteDirective,
     CategoryListPipe,
-    ProductItemFormComponent
   ],
   bootstrap: [
     AppComponent
   ]
 })
+
 export class AppModule {}
--- src/app/product-item-form.component.ts
+++ src/app/product-item-form.component.ts
@@ -1,8 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-product-item-form',
   templateUrl: './product-item-form.component.html',
   styleUrls: ['./product-item-form.component.css']
 })
-export class ProductItemFormComponent {}
+
+
+export class ProductItemFormComponent implements OnInit {
+  form: FormGroup;
+
+  ngOnInit(): void {
+      this.form = new FormGroup({
+        line: new FormControl('Movies'),
+        name: new FormControl('', Validators.compose([
+          Validators.required,
+          Validators.pattern('[\\w\\-\\s\\/]+')
+        ])),
+        category: new FormControl(''),
+        year: new FormControl('', this.yearValidator),
+      });
+  }
+
+  yearValidator(control: FormControl) {
+    if (control.value.trim().length === 0) {
+      return null;
+    }
+    const year = parseInt(control.value, 10);
+    const minYear = 1900;
+    const maxYear = 2100;
+    if (year >= minYear && year <= maxYear) {
+      return null;
+    } else {
+      return {
+        year: {
+          min: minYear,
+          max: maxYear
+        }
+      };
+    }
+  }
+
+  onSubmit(productItem) {
+    console.log(productItem);
+  }
+
+}
